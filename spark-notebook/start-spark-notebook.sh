13a9a6b5125ec8d470818adfbdad4dcdef2c1741
Dean Wampler <dean@concurrentthought.com>
2016-09-18 21:45:55 -0500

    Saving work on Spark Notebook and Jupyter/Toree versions.

---
+++ spark-notebook/start-spark-notebook.sh
@@ -0,0 +1,2 @@
+ docker pull andypetrella/spark-notebook:0.6.3-scala-2.11.7-spark-1.6.2-hadoop-2.7.2-with-hive-with-parquet
+ docker run -p 9000:9000 andypetrella/spark-notebook:0.6.3-scala-2.11.7-spark-1.6.2-hadoop-2.7.2-with-hive-with-parquet
