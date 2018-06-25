e848df824029857549d50331b18f2bac2943363f
Israel Zúñiga de la Mora <israelzdlm@gmail.com>
2018-06-24 22:37:32 -0500

    Fixing:   run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p 8888:8888 -p 4040:4040 -v $PWD:/home/jovyan/work jupyter/pyspark-notebook start-notebook.sh
+docker run -it --rm -p 8888:8888 -p 4040:4040 -v $PWD:/home/jovyan/work jupyter/all-spark-notebook start-notebook.sh
