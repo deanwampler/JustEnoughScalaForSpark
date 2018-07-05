44563e6c8e81595e490f15eb482f60f547218bb2
Dean Wampler <dean@concurrentthought.com>
2018-07-05 08:27:49 -0500

    Added BeakerX as a notebook option. Refined the instructions in the README.

--- run.sh
+++ run.sh
@@ -1,6 +1,41 @@
-docker run -it --rm \
-  -p 8888:8888 -p 4040:4040 \
-  --cpus=2.0 --memory=2000M \
-  -v "$PWD":/home/jovyan/work \
-  "$@" \
-  jupyter/all-spark-notebook
+#!/usr/bin/env bash
+
+help() {
+  cat <<EOF
+  $0: Run Jupyter (or BeakerX) for JustEnoughScalaForSpark
+
+  usage: $0 [-h | --help] [-b | --beakerx] [docker_options]
+
+  where:
+  -h | --help      Print this message and exit.
+  -b | --beakerx   Use BeakerX (http://beakerx.com) instead of Jupyter, the default.
+  docker_options   All other arguments are passed as arguments to the docker command.
+
+EOF
+}
+
+home=/home/jovyan
+image=jupyter/all-spark-notebook
+docker_options=()
+while [ $# -gt 0 ]
+do
+  case $1 in
+    -h|--h*)
+      help
+      exit 0
+      ;;
+    -b|--beak*)
+      home=/home/beakerx
+      image=beakerx/beakerx
+      echo "Using BeakerX"
+      ;;
+    *)
+      docker_options=(${docker_options[@]} $1)
+      ;;
+  esac
+  shift
+done
+
+# Invoke this script with NOOP=echo run.sh ... to see the command but not run it:
+$NOOP docker run -it --rm -p 8888:8888 -p 4040:4040 --cpus=2.0 --memory=2000M \
+  -v "$PWD":$home/$(basename $PWD) "${docker_options[@]}" $image
